c2f4f0540ef25fc5122b74b2d021a838d73a08f1
akizora001 <cometogetherrightnow1@gmail.com>
2022-02-01 00:48:58 +0900

    add new file

---
+++ tiktokApi.py
@@ -0,0 +1,24 @@
+from TikTokApi import TikTokApi
+
+verifyfp = "クッキーを指定"
+api = TikTokApi.get_instance(custom_verifyFp=verifyfp)
+
+# 特定のハッシュタグを持つ動画を10個取得
+results = 10
+hashtag = 'apple'
+tiktoks = api.by_hashtag(count=results, hashtag=hashtag)
+
+# 各動画の情報をループ処理で取得
+for tiktok in tiktoks:
+    print(tiktok])
+
+# 動画のダウンロード
+video_bytes = api.get_video_by_tiktok(tiktoks[0])
+video_title = tiktoks[0]["id"]
+
+# urlを指定して動画をダウンロードする場合は以下を使用
+# video_bytes = api.get_video_by_url('https://www.tiktok.com/@clearlyhemo/video/7032985960992361730?is_copy_url=1&is_from_webapp=v1&q=Messi&t=1638852369929')
+
+# 動画の保存
+with open(f"{video_title}.mp4", "wb") as o:
+    o.write(video_bytes)
