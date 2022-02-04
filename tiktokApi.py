7dad733f648707d651814847e56aa20ae60ee73f
akizora001 <cometogetherrightnow1@gmail.com>
2022-02-04 23:59:16 +0900

    fix

--- tiktokApi.py
+++ tiktokApi.py
@@ -1,24 +1,34 @@
+import sys
+import os
+import time
+from dotenv import load_dotenv
 from TikTokApi import TikTokApi
 
-verifyfp = "3819b87d-09f2-44f5-a076-fa98f64be896"
-api = TikTokApi.get_instance(custom_verifyFp=verifyfp)
+
+# load_dotenv(verbose=True)
+# dotenv_path = os.path.dirname('.env')
+# load_dotenv(dotenv_path)
+
+verifyfp = ''
+api = TikTokApi.get_instance(use_api_endpoints = True, custom_verifyFp=verifyfp)
 
 # 特定のハッシュタグを持つ動画を10個取得
 results = 10
-hashtag = 'エール'
+hashtag = 'エバーテイル'
 tiktoks = api.by_hashtag(count=results, hashtag=hashtag)
 
 # 各動画の情報をループ処理で取得
 for tiktok in tiktoks:
     print(tiktok)
+    # sys.exit()
+    # 動画のダウンロード
+    video_bytes = api.get_video_by_tiktok(tiktok)
+    video_title = tiktok["id"]
 
-# 動画のダウンロード
-video_bytes = api.get_video_by_tiktok(tiktoks[0])
-video_title = tiktoks[0]["id"]
+    # urlを指定して動画をダウンロードする場合は以下を使用
+    # video_bytes = api.get_video_by_url('https://www.tiktok.com/@clearlyhemo/video/7032985960992361730?is_copy_url=1&is_from_webapp=v1&q=Messi&t=1638852369929')
 
-# urlを指定して動画をダウンロードする場合は以下を使用
-# video_bytes = api.get_video_by_url('https://www.tiktok.com/@clearlyhemo/video/7032985960992361730?is_copy_url=1&is_from_webapp=v1&q=Messi&t=1638852369929')
-
-# 動画の保存
-with open(f"{video_title}.mp4", "wb") as o:
-    o.write(video_bytes)
+    # 動画の保存
+    with open(f"{video_title}.mp4", "wb") as o:
+        o.write(video_bytes)
+    time.sleep(2)
